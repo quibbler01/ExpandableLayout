57c1aa60fcda3aa69981cf303c2a00ee2900ab21
quibbler01 <fluent0418@gmail.com>
2022-12-19 12:17:22 +0800

    crate ExpandableLayout class

---
+++ ExpandableLayout/src/main/java/cn/quibbler/expandablelayout/ExpandableLayout.kt
@@ -0,0 +1,16 @@
+package cn.quibbler.expandablelayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class ExpandableLayout : LinearLayout {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+
+
+}
