ed9a9551b59f573ba5599be5b137ce0770338df7
quibbler01 <fluent0418@gmail.com>
2022-12-21 09:16:48 +0800

    ExpandableLayout/src/main/java/cn/quibbler/expandablelayout/ExpandableLayout.k

--- ExpandableLayout/src/main/java/cn/quibbler/expandablelayout/ExpandableLayout.kt
+++ ExpandableLayout/src/main/java/cn/quibbler/expandablelayout/ExpandableLayout.kt
@@ -2,15 +2,67 @@ package cn.quibbler.expandablelayout
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
+import androidx.annotation.LayoutRes
 
 class ExpandableLayout : LinearLayout {
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+    companion object {
 
+        private const val NO_RES = 0
 
+        private const val NO_INDEX = -1
+
+        private val DEFAULT_FILTER: Operation = object : Operation {
+            override fun apply(obj: Any?): Boolean = true
+        }
+
+    }
+
+    interface Renderer<P, C> {
+        fun renderParent(view: View, model: P, isExpand: Boolean, parentPosition: Int)
+
+        fun renderChild(view: View, model: C, parentPosition: Int, childPosition: Int)
+    }
+
+    private var layoutInflater: LayoutInflater? = null
+
+    @LayoutRes
+    private var parentLayout = -1
+
+    @LayoutRes
+    private var childLayout = -1
+
+    private var renderer: Renderer<*, *>? = null
+
+    private var sections: List<Section<*, *>>? = null
+
+    private var expandListener: ExpandCollapseListener.ExpandListener<*>? = null
+
+    private var collapseListener: ExpandCollapseListener.CollapseListener<*>? = null
+
+    private var currentFilter: Operation = DEFAULT_FILTER
+
+    constructor(context: Context?) : super(context) {
+        init(context, null)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context?, attrs: AttributeSet?) {
+
+    }
 
 }
